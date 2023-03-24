1b6fed3c0558b3d967eb1b9115dafee01fd3c5e1
Aditya ray <127591154+Adijalaxmi07@users.noreply.github.com>
2023-03-24 14:16:41 +0530

    day3 assignment submitted

---
+++ javascript assignment/index.js
@@ -0,0 +1,9 @@
+var arr=[2,5,6,8,12,13,45,27,90,276,134,567,709];
+function lovethethree() {
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%3==0){
+            console.log(arr[i]);
+        }
+    }
+}
+lovethethree();
